1b32e9f618cae7f467e13f39411bc72ee6856326
saci zakaria <sacideveloppeur@gmail.com>
2022-01-10 18:52:43 +0100

    ArchivedTasksScreen

---
+++ lib/models/archived_tasks_screen.dart
@@ -0,0 +1,14 @@
+import 'dart:html';
+
+import 'package:flutter/material.dart';
+
+class ArchivedTasksScreen extends StatelessWidget {
+  const ArchivedTasksScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text("ArchivedTasksScreen "),
+    );
+  }
+}
