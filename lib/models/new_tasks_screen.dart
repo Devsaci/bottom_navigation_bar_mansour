7618d4a0e63f7e435db072ade54bb6d8d20ca11c
saci zakaria <sacideveloppeur@gmail.com>
2022-01-10 18:51:10 +0100

    DoneTasksScreen

--- lib/models/new_tasks_screen.dart
+++ lib/models/new_tasks_screen.dart
@@ -1,9 +1,11 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'dart:html';
 
 import 'package:flutter/material.dart';
 
-class NewTasksScreen  extends StatelessWidget {
-  const NewTasksScreen ({Key? key}) : super(key: key);
+class NewTasksScreen extends StatelessWidget {
+  const NewTasksScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
