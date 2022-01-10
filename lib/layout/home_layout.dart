c46a116e0756752630571a8ddaee67614811845c
saci zakaria <sacideveloppeur@gmail.com>
2022-01-10 11:10:18 +0100

    BottomNavigationBarItem /   Icons.menu

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -17,7 +17,15 @@ class HomeLayout extends StatelessWidget {
         child: Icon(Icons.add_a_photo),
       ),
       bottomNavigationBar: BottomNavigationBar(
-        items: [],
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.menu,
+            ),
+            label: 'Tasks',
+          ),
+
+        ],
       ),
     );
   }
