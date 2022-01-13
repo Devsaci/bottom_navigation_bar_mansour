221f4b1820685c088ebb98d2c1a4d7e676f2621f
saci zakaria <sacideveloppeur@gmail.com>
2022-01-13 16:39:34 +0100

    then

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -128,7 +128,7 @@ class _HomeLayoutState extends State<HomeLayout> {
   void insertToDatabase() {
     database.transaction((txn)
     {
-      txn.rawInsert("sql");
+      txn.rawInsert("sql").then((value) => null);
       return null;
     });
   }
