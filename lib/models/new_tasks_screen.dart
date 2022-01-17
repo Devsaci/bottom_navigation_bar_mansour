3de5d8f4dadc4f9901b1cbf51111ca0765a7c3b4
saci zakaria <sacideveloppeur@gmail.com>
2022-01-17 18:20:26 +0100

    CircleAvatar

--- lib/models/new_tasks_screen.dart
+++ lib/models/new_tasks_screen.dart
@@ -3,13 +3,11 @@
 import 'package:flutter/material.dart';
 
 class NewTasksScreen extends StatelessWidget {
-
-
   @override
   Widget build(BuildContext context) {
     return Row(
       children: [
-
+        CircleAvatar(),
       ],
     );
   }
