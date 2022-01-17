ed5d8a9278b5e9fb7c98c23a4bd5c98dbb39f702
saci zakaria <sacideveloppeur@gmail.com>
2022-01-17 18:19:43 +0100

    Row

--- lib/models/new_tasks_screen.dart
+++ lib/models/new_tasks_screen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_constructors
+// ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
 
 import 'package:flutter/material.dart';
 
@@ -7,8 +7,10 @@ class NewTasksScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text("NewTasksScreen"),
+    return Row(
+      children: [
+
+      ],
     );
   }
 }
