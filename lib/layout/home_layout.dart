a4d3b5c1fb714f073ebca76810659a559e6669a9
saci zakaria <sacideveloppeur@gmail.com>
2022-01-18 11:24:38 +0100

    conditional_builder_null_safety: ^0.0.6

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -54,7 +54,7 @@ class _HomeLayoutState extends State<HomeLayout> {
         title: Text(titles[currentIndex]),
         centerTitle: true,
       ),
-      body: tasks.isEmpty ? Center(child: CircularProgressIndicator()) :  screens[currentIndex],
+      body: screens[currentIndex],
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           // try {
