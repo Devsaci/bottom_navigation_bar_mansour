081cb7abe390af30672d42cb055d5482b32cfac9
saci zakaria <sacideveloppeur@gmail.com>
2022-01-21 16:38:44 +0100

    MyBlocObserver

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,19 @@
 // ignore_for_file: prefer_const_constructors
 
+import 'package:bloc/bloc.dart';
+import 'package:bottom_navigation_bar_mansour/shared/cubit/bloc_observer.dart';
 import 'package:flutter/material.dart';
 
 import 'layout/home_layout.dart';
 
 void main() {
+  BlocOverrides.runZoned(
+        () {
+      // Use cubits...
+    },
+    blocObserver: MyBlocObserver(),
+  );
+
   runApp(MyApp());
 }
 
