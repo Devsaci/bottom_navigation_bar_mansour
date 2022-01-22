70abeaecc173bfbc4ec26c208e7e210d85136f57
saci zakaria <sacideveloppeur@gmail.com>
2022-01-22 10:13:59 +0100

    BlocProvider

--- lib/shared/cubit/cubit.dart
+++ lib/shared/cubit/cubit.dart
@@ -1,6 +1,9 @@
 import 'package:bloc/bloc.dart';
 import 'package:bottom_navigation_bar_mansour/shared/cubit/states.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class AppCubit extends Cubit<AppStates> {
   AppCubit() : super(AppInitialState());
+
+  static AppCubit get(context) => BlocProvider.of(context);
 }
