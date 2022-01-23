dc630a0bd0263ad904ac905e0535dafb0c696f30
saci zakaria <sacideveloppeur@gmail.com>
2022-01-23 10:13:35 +0100

    AppCreateDatabaseState/AppGetDatabaseState/AppInsertDatabaseState

--- lib/shared/cubit/states.dart
+++ lib/shared/cubit/states.dart
@@ -2,4 +2,7 @@ abstract class AppStates{}
 class AppInitialState extends AppStates {}
 class AppChangeBottomNavBarState extends AppStates {}
 class AppCreateDatabaseState  extends AppStates {}
+class AppGetDatabaseState extends AppStates {}
+class AppInsertDatabaseState extends AppStates {}
+
 
