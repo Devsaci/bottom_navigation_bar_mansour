918cf808825a947301dac2a8153568552733021e
saci zakaria <sacideveloppeur@gmail.com>
2022-01-23 10:21:31 +0100

    Stopped insertToDatabase / getDataFromDatabase

--- lib/layout/home_layout.dart
+++ lib/layout/home_layout.dart
@@ -15,7 +15,7 @@ import 'package:intl/intl.dart';
 import 'package:sqflite/sqflite.dart';
 
 class HomeLayout extends StatelessWidget {
-  late Database database;
+
   var scaffoldKey = GlobalKey<ScaffoldState>();
   late var formKey = GlobalKey<FormState>();
   bool isBottomSheetShown = false;
@@ -69,27 +69,27 @@ class HomeLayout extends StatelessWidget {
 
                   if (isBottomSheetShown) {
                     if (formKey.currentState!.validate()) {
-                      insertToDatabase(
-                        title: titleController.text,
-                        date: dateController.text,
-                        time: timeController.text,
-                      ).then((value) {
-                        getDataFromDatabase(database).then((value) {
-                          Navigator.pop(context);
-                          // setState(() {
-                          //   isBottomSheetShown = false;
-                          //   fabIcon = Icons.edit;
-                          //   tasks = value;
-                          //   if (kDebugMode) {
-                          //     print(tasks[0]);
-                          //     print(tasks[1]);
-                          //     print(tasks[2]);
-                          //     print(tasks[3]);
-                          //     print(tasks[4]);
-                          //   }
-                          // },);
-                        });
-                      });
+                      // insertToDatabase(
+                      //   title: titleController.text,
+                      //   date: dateController.text,
+                      //   time: timeController.text,
+                      // ).then((value) {
+                      //   getDataFromDatabase(database).then((value) {
+                      //     Navigator.pop(context);
+                      //     // setState(() {
+                      //     //   isBottomSheetShown = false;
+                      //     //   fabIcon = Icons.edit;
+                      //     //   tasks = value;
+                      //     //   if (kDebugMode) {
+                      //     //     print(tasks[0]);
+                      //     //     print(tasks[1]);
+                      //     //     print(tasks[2]);
+                      //     //     print(tasks[3]);
+                      //     //     print(tasks[4]);
+                      //     //   }
+                      //     // },);
+                      //   });
+                      // });
                     }
                   } else {
                     scaffoldKey.currentState
@@ -238,66 +238,9 @@ class HomeLayout extends StatelessWidget {
     );
   }
 
-  Future<String> getName() async {
-    return "Saci Zakaria";
-  }
+  // Future<String> getName() async {
+  //   return "Saci Zakaria";
+  // }
 
-  void createDatabase() async {
-    database = await openDatabase(
-      "todo.db",
-      version: 1,
-      // id integer
-      // title String
-      // date String
-      // time String
-      // status String
-      onCreate: (database, version) {
-        print("database created ");
-        database
-            .execute(
-            'CREATE TABLE tasks (id INTEGER PRIMARY KEY, title TEXT, date TEXT, time TEXT, status TEXT)')
-            .then((value) {
-          print("table created");
-        }).catchError((error) {
-          print("Error when Creating Table");
-        });
-      },
-      onOpen: (database) {
-        getDataFromDatabase(database).then((value) {
-          // setState(() {
-          //   tasks = value;
-          //   tasks = value;
-          // });
-          //
-          print(tasks[
-          0]); //{id: 1, title: go to swiming, date: Jan 18, 2022, time: 10:19, status: new}
-          print(tasks[
-          1]); //{id: 2, title: go to market, date: Jan 19, 2022, time: 11:20, status: new}
-        });
-        print("database opened ");
-      },
-    );
-  }
 
-  Future insertToDatabase({
-    required String title,
-    required String time,
-    required String date,
-  }) async {
-    return await database.transaction((txn) async {
-      txn
-          .rawInsert(
-        'INSERT INTO tasks( title, date, time, status) VALUES( "$title", "$date", "$time", "new")',
-      )
-          .then((value) {
-        print('$value Inserted Successfully');
-      }).catchError((error) {
-        print('Error When Inserting New Record ${error.toString()}');
-      });
-    });
-  }
-
-  Future<List<Map>> getDataFromDatabase(database) async {
-    return await database.rawQuery('SELECT * FROM tasks');
-  }
 }
