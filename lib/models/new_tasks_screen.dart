b26d4006b899a1d0382271afb35bea9204ec3dda
saci zakaria <sacideveloppeur@gmail.com>
2022-01-23 10:39:35 +0100

    Stopped NewTasksScreen

--- lib/models/new_tasks_screen.dart
+++ lib/models/new_tasks_screen.dart
@@ -1,4 +1,6 @@
 // ignore_for_file: prefer_const_constructors, prefer_const_literals_to_create_immutables
+import 'dart:html';
+
 import 'package:bottom_navigation_bar_mansour/shared/components/components.dart';
 import 'package:bottom_navigation_bar_mansour/shared/components/constants.dart';
 import 'package:flutter/cupertino.dart';
@@ -9,19 +11,22 @@ class NewTasksScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ListView.separated(
-      itemBuilder: (context, index) => buildTaskItem(tasks[index]),
-      separatorBuilder: (context, index) => Padding(
-        padding: const EdgeInsetsDirectional.only(
-          start: 20.0
-        ),
-        child: Container(
-          width: double.infinity,
-          height: 1,
-          color: Colors.indigo,
-        ),
-      ),
-      itemCount: tasks.length,
+    return Container(
+      child: Text("Stopped NewTasksScreen"),
     );
+    // return ListView.separated(
+    //   itemBuilder: (context, index) => buildTaskItem(tasks[index]),
+    //   separatorBuilder: (context, index) => Padding(
+    //     padding: const EdgeInsetsDirectional.only(
+    //       start: 20.0
+    //     ),
+    //     child: Container(
+    //       width: double.infinity,
+    //       height: 1,
+    //       color: Colors.indigo,
+    //     ),
+    //   ),
+    //   itemCount: tasks.length,
+    // );
   }
 }
