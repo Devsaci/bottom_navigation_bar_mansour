5dd97ccaa8081f40ead18b8d28d71810fc8576fa
saci zakaria <sacideveloppeur@gmail.com>
2022-01-27 18:09:58 +0100

    AppCubit,AppStates

--- lib/models/new_tasks_screen.dart
+++ lib/models/new_tasks_screen.dart
@@ -3,6 +3,7 @@
 import 'package:bottom_navigation_bar_mansour/shared/components/components.dart';
 import 'package:bottom_navigation_bar_mansour/shared/components/constants.dart';
 import 'package:bottom_navigation_bar_mansour/shared/cubit/cubit.dart';
+import 'package:bottom_navigation_bar_mansour/shared/cubit/states.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -10,7 +11,7 @@ import 'package:flutter_bloc/flutter_bloc.dart';
 class NewTasksScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return BlocConsumer(
+    return BlocConsumer<AppCubit,AppStates>(
       listener: (context, state) {},
       builder: (context, state) {
         var tasks = AppCubit.get(context).tasks;
